3f51afa35c977af66dc77145ad60c033ce43ad91
ganeshhappysanz <ganesh.happysanz@gmail.com>
2022-07-13 14:56:24 +0530

    Aftersuite added in base class

--- src/main/java/base/Baseclass.java
+++ src/main/java/base/Baseclass.java
@@ -3,26 +3,31 @@ package base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Baseclass {
-	
+
 	public static ThreadLocal<RemoteWebDriver> driver = new ThreadLocal<>();
-	
-	
-	public  static WebDriver getDriver() {
+
+	public static WebDriver getDriver() {
 		return driver.get();
-		
+
 	}
-	
+
 	@BeforeSuite
 	public void launchApp() {
 		WebDriverManager.chromedriver().setup();
-		driver.set(new ChromeDriver());	
-		
-		
+		driver.set(new ChromeDriver());
+	}
+
+	@AfterSuite
+	public void afterSuite() {
+		if (getDriver() != null) {
+			getDriver().quit();
+		}
 	}
 
 }
